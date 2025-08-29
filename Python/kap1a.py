a7937299b8b5e8272c82972311eee2b71d8d5c8d
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-08-29 10:16:18 +0200

    Added PySide6

---
+++ Python/kap1a.py
@@ -0,0 +1,29 @@
+#Formatering av tekst:
+"""
+import math as m
+
+radius = 8
+areal = m.pi*radius**2
+
+print(f"Arealet av sirkelen med {radius} i radius er {areal:.2f}.")
+
+Oppgave 28 kap 1A
+
+navn = "Andreas"
+alder = 18
+
+print(f"navn: {navn:>5}")
+print(f"alder: {alder:6}")
+
+"""
+
+tekst1 = "Monty"
+tekst2 = "Python"
+tekst3 = tekst1.upper()
+tekst4 = tekst2.lower()
+tekst5 = tekst3[:4]
+tekst6 = tekst4[4:]
+tekst7 = tekst5 + tekst6
+
+print(tekst7)
+
