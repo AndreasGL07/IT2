b6c2ad88975253963f1f22cddbcad831c9a05c93
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-09-18 08:31:34 +0200

    gregre

--- Python/news_app.py/button_holder.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton
-
-class ButtonHolder(QMainWindow):
-    def __init__(self):
-        super().__init__(self)
-        self.setWindowTitle("Button Holder app")
-        button = QPushButton("Press me!")
-        # Set our button as the sentral widget
-        self.setCentralWidget(button)
--- Python/news_app.py/main.py
+++ Python/news_app.py/main.py
@@ -1,12 +1,18 @@
 
 #The sys module is responsible for processing command line arguments
 import sys
-from PySide6.QtWidgets import QApplication
-from button_holder import ButtonHolder
+from PySide6.QtWidgets import QApplication, QPushButton
+
+def button_clicked():
+    print("You clicked the button")
 
 app = QApplication(sys.argv)
-window = ButtonHolder()
-window.show()
+button = QPushButton("Start")
+button.setCheckable(True) #Makes the button checkable. It's unchecked by default. Further clicks toggle between checked and unchecked states
 
-#Start the event loop
+
+#clicked is a signal for QPushButton. It's emitted when you click the button. You can wire a slot to the signal using the syntax below
+button.clicked.connect(button_clicked) 
+
+button.show()
 app.exec()
